bfa66e7135eecbbe096119a0ab1991cefb129a8f
Pasa Ali Aslan <pasaali1603@gmail.com>
2024-04-14 15:04:47 -0400

    Introduced themes

--- imors-frontend/src/context/ThemeContext.tsx
+++ imors-frontend/src/context/ThemeContext.tsx
@@ -1,1 +1,38 @@
-/* Placeholder File */
+import React, { FC, ReactNode, createContext, useContext, useState } from "react";
+
+type Theme = "light" | "dark";
+
+type ThemeContextType = {
+    theme: Theme;
+    toggleTheme: () => void;
+};
+
+type ThemeProviderProps = {
+    children: ReactNode;
+};
+
+const ThemeContext = createContext<ThemeContextType | undefined>(undefined);
+
+export const ThemeProvider: FC<ThemeProviderProps> = ({ children }) => {
+    const [theme, setTheme] = useState<Theme>("light");
+
+    const toggleTheme = () => {
+        setTheme((currentTheme) => {
+            const newTheme = currentTheme === "light" ? "dark" : "light";
+
+            document.body.className = newTheme;
+
+            return newTheme;
+        });
+    };
+
+    return <ThemeContext.Provider value={{ theme, toggleTheme }}>{children}</ThemeContext.Provider>;
+};
+
+export function useTheme(): ThemeContextType {
+    const context = useContext(ThemeContext);
+    if (context === undefined) {
+        throw new Error("useTheme must be used within a ThemeProvider");
+    }
+    return context;
+}
